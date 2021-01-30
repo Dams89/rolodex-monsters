7539ec14ef8267476060e5e36a0ffe0216382f4d
Dams89 <64008705+Dams89@users.noreply.github.com>
2021-01-30 14:57:31 +0100

    Project setup and first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string : 'Hello I am Damiano'
+    }
+  }
 
+  render() {
+    return(
+      <div className='App'>
+      <header class='header-App'>
+        <p> {this.state.string} </p>
+        <button onClick = {() => this.setState({string: '...and I love coding'})}> Click here </button>
+        </header>
+      </div>
+    )
+  }
+}
 export default App;
